52c882d26479bf588d076dc2965f8ed7fd6b969b
yassinnho7 <yassin.nho7@gmail.com>
2026-02-03 18:58:37 +0100

    🚀 Mobile Super Experience: Auto-Fullscreen, Landscape, and Adsterra Restoration

--- public/js/monetization-v2.js
+++ public/js/monetization-v2.js
@@ -43,23 +43,45 @@ class MonetizationManager {
     initAdsterra() {
         const container = document.getElementById('adsterra-banner-container');
         if (container && this.config.adsterra.banner) {
-            const div = document.createElement('div');
-            div.innerHTML = this.config.adsterra.banner;
-            Array.from(div.querySelectorAll('script')).forEach(s => {
-                const ns = document.createElement('script');
-                Array.from(s.attributes).forEach(a => ns.setAttribute(a.name, a.value));
-                ns.innerHTML = s.innerHTML;
-                container.appendChild(ns);
-            });
+            console.log('📡 Injecting Adsterra Banner...');
+            const bannerHtml = this.config.adsterra.banner;
+
+            if (bannerHtml.includes('atOptions')) {
+                const keyMatch = bannerHtml.match(/'key'\s*:\s*['"]([^'"]+)['"]/);
+                const key = keyMatch ? keyMatch[1] : '129087cf28b991b1ac22461099f7d24c';
+
+                window.atOptions = {
+                    'key': key,
+                    'format': 'iframe',
+                    'height': 60,
+                    'width': 468,
+                    'params': {}
+                };
+
+                const script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.src = `//www.highperformanceformat.com/${key}/invoke.js`;
+                container.appendChild(script);
+            } else {
+                container.innerHTML = bannerHtml;
+            }
         }
 
         if (this.config.adsterra.socialBarKey) {
-            const script = document.createElement('script');
-            const srcMatch = this.config.adsterra.socialBarKey.match(/src=["']([^"']+)["']/);
-            if (srcMatch) {
+            console.log('📡 Injecting Social Bar...');
+            const socialContent = this.config.adsterra.socialBarKey;
+            const srcMatch = socialContent.match(/src=["']([^"']+)["']/);
+
+            if (srcMatch && srcMatch[1]) {
+                const script = document.createElement('script');
+                script.type = 'text/javascript';
                 script.src = srcMatch[1];
                 script.async = true;
                 document.body.appendChild(script);
+            } else {
+                const div = document.createElement('div');
+                div.innerHTML = socialContent;
+                document.body.appendChild(div);
             }
         }
     }
